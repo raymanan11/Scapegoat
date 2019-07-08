cc15ac2ec9d164e50062c131af53ff9380c44bcc
Raymond An <raymondan100@gmail.com>
2019-07-07 22:28:44 -0700

    Finished Terrain and added Splash Screen and Background Music

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+    [SerializeField] float levelLoadDelay;
+    // Start is called before the first frame update
+    void Start() {
+        PlayNextLevel();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    private void PlayNextLevel () {
+        Invoke("LoadNextLevel", levelLoadDelay);
+    }
+
+    void LoadNextLevel() {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;  // tells you what index level you are on
+        int nextSceneIndex = currentSceneIndex + 1;
+        int lastLevelIndex = SceneManager.sceneCountInBuildSettings;
+        print(currentSceneIndex);
+        if (nextSceneIndex == lastLevelIndex) {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
