619ccab0f30f1b67e003c61342dcbb10dbf0cb83
Raymond An <raymondan100@gmail.com>
2019-07-08 21:09:34 -0700

    Made the spacecraft move left and right while restricting the x transform

--- Assets/Spacecraft.cs
+++ Assets/Spacecraft.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
-public class Spacecraft : MonoBehaviour
-{
+public class Spacecraft : MonoBehaviour {
+
+    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 4f;
+    [SerializeField] float xMax = 15f;
+    [SerializeField] float xMin = -15f;
+
     // Start is called before the first frame update
     void Start() {
         
@@ -12,7 +16,11 @@ public class Spacecraft : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
-        float horizontalMvmt = CrossPlatformInputManager.GetAxis("Horizontal"); // generic input for keyboard and controller
-        print(horizontalMvmt);
+        float xMvmt = CrossPlatformInputManager.GetAxis("Horizontal"); // generic horizontal input for keyboard and controller
+        float xOffset = xMvmt * xSpeed * Time.deltaTime;
+        float rawXPos = transform.localPosition.x + xOffset; //changes the local x transform of the Spacecraft but doesn't restrict movement
+        float xPos = Mathf.Clamp(rawXPos, xMin, xMax); //restricts the movement of the spacecraft to only move from -15 to 15 units
+
+        transform.localPosition = new Vector3(xPos, transform.localPosition.y, transform.localPosition.z);
     }
 }
