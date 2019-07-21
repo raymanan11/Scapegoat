95800528f235fb9fd37a5a2375ff79ac8c177597
Raymond An <raymondan100@gmail.com>
2019-07-20 18:09:06 -0700

    Added working game score as fighter jet hits enemies

--- Assets/Scripts/EnemySpacecrafts.cs
+++ Assets/Scripts/EnemySpacecrafts.cs
@@ -7,10 +7,15 @@ public class EnemySpacecrafts : MonoBehaviour
 {
     [SerializeField] GameObject enemyDeathFX;
     [SerializeField] Transform parent;
+    [SerializeField] int scorePerShot = 5;
+
+    GameScore gameScore; // a reference to the GameScore script
+
     // Start is called before the first frame update
     void Start()
     {
         AddBoxCollider();
+        gameScore = FindObjectOfType<GameScore>(); // every enemy is finding the reference of the GameScore script (Enemy and GameScore communicating)
     }
 
     private void AddBoxCollider() {
@@ -19,6 +24,7 @@ public class EnemySpacecrafts : MonoBehaviour
     }
 
     void OnParticleCollision(GameObject other) {
+        gameScore.Score(scorePerShot); // access the function from GameScore script
         GameObject deathFX = Instantiate(enemyDeathFX, transform.position, Quaternion.identity);// Before destroying the enemy, there has to be an explosion dropped or instantiated 
                                                                                                 //at the position of enemy with no roation
         deathFX.transform.parent = parent;
---
+++ Assets/Scripts/GameScore.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameScore : MonoBehaviour
+{
+
+    int score = 0;
+    Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = GetComponent<Text>(); // gets the text component 
+        scoreText.text = score.ToString(); // the .text gets the actual string in the text box and makes it equal to our score variable
+    }
+
+    public void Score(int scorePerShot) { // allows other classes to call this function in order to update the score
+        score = score + scorePerShot;
+        scoreText.text = score.ToString();
+    }
+}
